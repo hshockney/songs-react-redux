382e1dfe897c2784f400fe5ab418bd6e05374d35
hshockney <shockney@atlanticbb.net>
2019-06-25 11:09:58 -0400

    Created two reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+   return [
+        { title: 'I Want it That Way', duration: '4:05'},
+        { title: 'Sweet Caroline', duration: '2:45'},
+        { title: 'Shallows', duration: '3:25'},
+        { title: 'Pour Some Sugar On Me', duration: '3:45'}
+   ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+    return selectedSong;
+};
